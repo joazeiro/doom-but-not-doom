62f558bac5a4ea7528e57cfe505ed17d7d37aecc
Duda <eduardajoazeiro@knights.ucf.edu>
2023-04-05 10:25:37 -0400

    finish

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
-# library imports
-import pygame
+# all imports needed
+import pygame as pg
 import sys
-
-# file imports
 from settings import *
 from map import *
+from player import *
+from raycasting import *
+from object_renderer import *
+from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
+from pathfinding import *
 
+
+# declaration of game class
 class Game:
-    def __init__(self) -> None:
-        pygame.init()
-        self.screen = pygame.display.set_mode(RES)
-        self.clock = pygame.time.Clock()
+    def __init__(self):
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.screen = pg.display.set_mode(RES) # use resolution from settings
+        pg.event.set_grab(True)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
+        self.new_game() # start new game everytime we enter the game
 
-    def start_new(self):
-        pass
+        # TO-DO: 
+        # 1. add menu
+        # 2. fix music issue
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
+        self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
+        #pg.mixer.music.play(-1)
 
     def update(self):
-        pygame.display.flip()
-        self.clock.tick(FPS)
-        pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
+        self.player.update()
+        self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption('Doom But Not Doom')
 
     def draw(self):
-        self.screen.fill('blue')
+        self.object_renderer.draw()
+        self.weapon.draw()
 
-    def run_game(self):
+    def check_events(self):
+        self.global_trigger = False
+        for event in pg.event.get():
+            # if you hit ask or the X, exit of the game
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+            self.player.single_fire_event(event)
+
+    def run(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
+            self.check_events()
             self.update()
             self.draw()
 
+
 if __name__ == '__main__':
     game = Game()
-    game.run_game()
+    game.run()
